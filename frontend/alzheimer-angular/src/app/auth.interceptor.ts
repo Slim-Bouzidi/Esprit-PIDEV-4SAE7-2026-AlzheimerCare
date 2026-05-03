9eb8539d824a2a0ecfc1109344aaf9e3d6db2cd6
Slim-Bouzidi <Sliim.Bouzidi@esprit.tn>
2026-05-04 00:24:25 +0100

    Fix auth interceptor: use updateToken to ensure token is always fresh

--- frontend/alzheimer-angular/src/app/auth.interceptor.ts
+++ frontend/alzheimer-angular/src/app/auth.interceptor.ts
@@ -1,19 +1,21 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import { catchError, throwError } from 'rxjs';
+import { from, switchMap, catchError, throwError } from 'rxjs';
 import keycloak from './keycloak';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = keycloak.token;
-
-  if (token) {
-    req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
+  // Refresh the token if it will expire in the next 30 seconds
+  return from(keycloak.updateToken(30)).pipe(
+    switchMap(() => {
+      const token = keycloak.token;
+      if (token) {
+        req = req.clone({
+          setHeaders: {
+            Authorization: `Bearer ${token}`
+          }
+        });
       }
-    });
-  }
-
-  return next(req).pipe(
+      return next(req);
+    }),
     catchError((error) => {
       if (error.status === 401) {
         keycloak.login();
