fdf9862b496b0848adafdb0c11cef2f4377969ea
Slim-Bouzidi <Sliim.Bouzidi@esprit.tn>
2026-05-07 02:26:08 +0100

    clean: remove broken tests

--- backend/doctor-service/src/test/java/com/alzheimer/supportnetwork/TestSpringBootConfiguration.java
+++
@@ -1,6 +0,0 @@
-package com.alzheimer.supportnetwork;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages = {"org.example.alzheimerapp", "com.alzheimer.supportnetwork"})
-public class TestSpringBootConfiguration {}
